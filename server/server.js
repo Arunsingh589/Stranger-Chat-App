30bb013233ed14ac113058cbac90e15af8e12587
Arun Singh <as3945787@gmail.com>
2024-10-05 21:09:29 +0530

    vercel

--- server/server.js
+++ server/server.js
@@ -10,10 +10,15 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",
-        methods: ["GET", "PUT"]
+        methods: ["GET", "PUT"],
+        credentials: true
     }
 });
 
+
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
 
 const users = {};
 
