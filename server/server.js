3f5d98cb44b74923f73ce752225a10eeebdc7ea2
Arun Singh <126667359+Arunsingh589@users.noreply.github.com>
2024-10-05 21:38:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://stranger-chat-application-frontend.vercel.app/",
         methods: ["GET", "PUT"],
         credentials: true
     }
