e9fada8b6d72c31895d8bb94bb003af8005b38df
Arun Singh <126667359+Arunsingh589@users.noreply.github.com>
2024-11-11 22:49:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,8 @@ const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",
         methods: ["GET", "PUT"]
-    }
+    },
+    transports: ["polling"],  // Force Socket.io to use HTTP long-polling
 });
 
 
@@ -89,7 +90,7 @@ io.on("connection", (socket) => {
 
 app.use(cors())
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
 
 
